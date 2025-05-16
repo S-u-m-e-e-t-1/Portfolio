c1ce3106b95d148a9dc7bda51879756506c52fb5
Sumeet Panigrahy <128874903+S-u-m-e-e-t-1@users.noreply.github.com>
2025-05-16 19:51:27 +0530

    images path

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/image/HexaPic.png"
+        src="/images/HexaPic.png"
         alt="Sumeet Panigrahy"
         className="max-h-[450px] w-auto"
       />
